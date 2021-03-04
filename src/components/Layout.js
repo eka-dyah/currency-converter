d24dec3e2f163db4127a7a6d74025b76f91f1f65
eka-dyah <eka.dya1205@gmail.com>
2021-03-04 23:05:17 +0700

    edit max width to 500

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,10 @@ import Header from "./Header";
 
 const Layout = ({ children }) => (
 	<>
-        <Header title="Currency Converter" />
-		<div className="container-sm">{children}</div>
+		<Header title="Currency Converter" />
+		<div className="container-sm" style={{ maxWidth: 500 }}>
+			{children}
+		</div>
 	</>
 );
 

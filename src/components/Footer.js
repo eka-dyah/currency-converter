711badad05b4984fa2b45f3c7c1c05727c2b21b3
eka-dyah <eka.dya1205@gmail.com>
2021-03-27 21:44:35 +0700

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => (
+	<footer
+		className="py-2 position-absolute bottom-0 text-center w-100 bg-secondary"
+		style={{ bottom: 0 }}
+	>
+		<p className="m-0 text-white">Made by Dyah Eka Lestari, 2021</p>
+	</footer>
+);
+
+export default Footer;
